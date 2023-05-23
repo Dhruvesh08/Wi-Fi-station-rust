03081ae820712757d2430d4311f930be32e8e759
Dhruvesh <dhruvesh.baria@actonate.com>
2023-05-24 01:57:53 +0530

    wifi-station

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use env_logger::Env;
+use log::{error, info};
+use wifi_station::{sta, Result};
+
+#[tokio::main]
+async fn main() -> Result {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    info!("Starting wifi-sta example");
+
+    let mut setup = sta::WifiSetup::new()?;
+    // Use something like ifconfig to figure out the name of your WiFi interface
+    setup.set_socket_path("/var/run/wpa_supplicant/wlp2s0");
+
+    let broadcast = setup.get_broadcast_receiver();
+    let requester = setup.get_request_client();
+    let runtime = setup.complete();
+
+    let (_runtime, _app, _broadcast) = tokio::join!(
+        async move {
+            if let Err(e) = runtime.run().await {
+                error!("Error: {e}");
+            }
+        },
+        app(requester),
+        broadcast_listener(broadcast),
+    );
+    Ok(())
+}
+
+async fn app(requester: sta::RequestClient) -> Result {
+    info!("Requesting scan");
+    let scan = requester.get_scan().await?;
+    info!("Scan complete");
+    for scan in scan.iter() {
+        info!("   {:?}", scan);
+    }
+
+    let networks = requester.get_networks().await?;
+    info!("Known networks");
+    for networks in networks.iter() {
+        info!("   {:?}", networks);
+    }
+    info!("Shutting down");
+    requester.shutdown().await?;
+    Ok(())
+}
+
+async fn broadcast_listener(mut broadcast_receiver: sta::BroadcastReceiver) -> Result {
+    while let Ok(broadcast) = broadcast_receiver.recv().await {
+        info!("Broadcast: {:?}", broadcast);
+    }
+    Ok(())
+}
