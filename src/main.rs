e1abecabde2459115b297b3139f21b3dc2cba96d
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-05-31 12:51:30 +0530

    wifi-connect-test

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,10 @@ async fn app(requester: sta::RequestClient) -> Result {
     info!("Known networks");
     for networks in networks.iter() {
         info!("   {:?}", networks);
+
+        let wifi_ssid = "Actonate_Tenda_2.4";
+        let wifi_psk = "actonate9522";
+        connect_to_network(&requester, wifi_ssid, wifi_psk).await?;
     }
     info!("Shutting down");
     requester.shutdown().await?;
@@ -49,5 +53,24 @@ async fn broadcast_listener(mut broadcast_receiver: sta::BroadcastReceiver) -> R
     while let Ok(broadcast) = broadcast_receiver.recv().await {
         info!("Broadcast: {:?}", broadcast);
     }
+    Ok(())
+}
+
+async fn connect_to_network(requester: &sta::RequestClient, ssid: &str, psk: &str) -> Result {
+    // Add a new network
+    let network_id = requester.add_network().await?;
+    info!("Added network: {:?}", network_id);
+
+    // Set the SSID and PSK for the network
+    requester
+        .set_network_ssid(network_id, ssid.to_string())
+        .await?;
+    requester
+        .set_network_psk(network_id, psk.to_string())
+        .await?;
+
+    // Select the network
+    requester.select_network(network_id).await?;
+
     Ok(())
 }
